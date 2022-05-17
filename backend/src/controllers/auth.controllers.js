bd531c5d6ac56081f8ae0409cd3208ee275a9e9e
itsnicolearias <nicolmccann@gmail.com>
2022-05-17 11:18:23 -0300

    feat: add authentication, hash password and token

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,10 +1,68 @@
 const User = require('../models/users.models')
+const bcrypt = require('bcrypt')
+const JWT = require('jsonwebtoken')
+const enviromentConfig = require('../config/environmentConfig')
 
 const register = async(req, res) => {
+   
+    const { name, age, email, password } = req.body
+     
+   try {
+     //check if email already exist
+     const emailExist = await User.findOne({ email: email})
+     if (emailExist) {
+         throw new Error("User already exists");
+     }
+
+     //hash password
+     const salts = await bcrypt.genSalt(10);
+     const hashedPassword = await bcrypt.hash(password, salts)
+
+     const user = new User({
+        name: name,
+        age: age,
+        email: email,
+        password: hashedPassword
+    })
+    const userSaved = await user.save()
+    res.json({ message: 'user registered', userSaved})
+
+   } catch (error) {
+    throw new Error(error);
+   }
+   
+
 
 }
 
 const login = async(req, res) => {
+    
+    const { email, password } = req.body
+
+    try {
+        //check if the user exist in the database
+        const user = await User.findOne({ email: email })
+        if(!user)  {
+            throw new Error("User doesnt exists");
+        } 
+
+         //compare password
+         const validPass = await bcrypt.compare(password, user.password)
+         if (!validPass) {
+             throw new Error("Invalid credentials");
+         }
+
+          //JWT
+          const token = JWT.sign({
+            name: user.name,
+            id: user._id
+        }, enviromentConfig.jwt.secret)
+
+        res.json({message: 'User logged sucessfully', token })
+    } catch (error) {
+        throw new Error(error);
+    }
+
 
 }
 
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const burgerRoutes = require('./burgers.routes')
+const authRoutes = require('./auth.routes')
 
 const router = express.Router()
 
 router.use('/', burgerRoutes)
+
+router.use('/auth', authRoutes)
 
 module.exports = router;
